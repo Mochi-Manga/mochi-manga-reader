f323dc8a2b180f429d34211b9cea2e39f9eb670a
James Lee <lee.jamests@gmail.com>
2022-04-19 20:53:06 -0400

    Axios call update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { useEffect, useState } from 'react';
 import BasicCard from './components/MangaCard';
 import Navbar from './components/Navbar';
 import MangaApi from './services/MangaAPI';
-import axios from 'axios';
 
 export default function App(props) {
   const [data, setData] = useState({});
   const fetchData = async () => {
-      const response = await axios.get(`https://kitsu.io/api/edge/manga/1`);
+      const response = await MangaApi();
+      console.log(response);
       let synopsis = response.data.data.attributes.synopsis;
       let poster = response.data.data.attributes.posterImage.medium;
       let title = response.data.data.attributes.canonicalTitle;
@@ -17,7 +17,6 @@ export default function App(props) {
         'synopsis': synopsis,
         'posterURL': poster
       }
-      console.log(response);
       setData(dataObject);
   };
   useEffect(() => {
--- src/services/MangaAPI.js
+++ src/services/MangaAPI.js
@@ -2,12 +2,10 @@ import React from 'react';
 import axios from 'axios';
 
 
-async function MangaApi(props) {
+async function MangaApi() {
   try {
     const response = await axios.get(`https://kitsu.io/api/edge/manga/1`);
-    let synopsis = toString(response.data.data.attributes.synopsis);
-    let poster = response.data.data.attributes.posterImage.medium;
-    console.log(synopsis, poster)
+    return response;
   } catch(e) {
     console.log('error has occurred: ' + e)
   }
