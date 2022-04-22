9d63be3a176ecb56c4f16f6756fc56f3aa75ea47
James Lee <lee.jamests@gmail.com>
2022-04-22 19:08:28 -0400

    Initial save function upload

--- src/services/MangaAPI.js
+++ src/services/MangaAPI.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 async function MangaApi() {
   try {
-    const response = await axios.get(`https://kitsu.io/api/edge/manga/1`);
+    const response = await axios.get(`https://kitsu.io/api/edge/manga/`);
     return response;
   } catch(e) {
     console.log('error has occurred: ' + e)
